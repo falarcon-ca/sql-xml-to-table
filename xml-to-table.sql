426dbcbbf5a7eeea25ff521b35792dce21ed7191
Fabian Alarcon <114283556+falarcon-ca@users.noreply.github.com>
2022-11-06 01:28:20 -0500

    Update xml-to-table.sql

--- xml-to-table.sql
+++ xml-to-table.sql
@@ -1,13 +1,11 @@
 DECLARE @xmlExample XML = '
 <Root>
-	<Element attrId="1" attrDescription="Text 1" attrNumber="01234"></Element>
-	<Element attrId="2" attrDescription="Text 2" attrNumber="56789"></Element>
+    <Element attrId="1" attrDescription="Text 1" attrNumber="01234"></Element>
+    <Element attrId="2" attrDescription="Text 2" attrNumber="56789"></Element>
 </Root>
 '
 
-SELECT
-	Result.item.value('@attrId', 'INT') AS [Id],
-	Result.item.value('@attrDescription', 'VARCHAR(500)') AS [Description],
-	Result.item.value('@attrNumber', 'INT') AS [Number]
-FROM
-	@xmlExample.nodes('/Root/Element') Result(item)
+SELECT  Result.item.value('@attrId', 'INT') AS [Id],
+        Result.item.value('@attrDescription', 'VARCHAR(500)') AS [Description],
+        Result.item.value('@attrNumber', 'INT') AS [Number]
+FROM    @xmlExample.nodes('/Root/Element') Result(item)
